4f9a36b886997302800f39d3f18d6d2fc8e00b30
pworontzoff <pierre.worontzoff@hepl.be>
2023-04-16 16:02:00 +0200

    Add files via upload

--- c_dragon_5/main.c
+++ c_dragon_5/main.c
@@ -8,7 +8,27 @@
 int main() {
 	init_drawing(LARGEUR,HAUTEUR);
 
+
+
+    draw(100);
+
+	turn(90,RIGHT);
 	
+	draw(100);
+	
+	turn(90,LEFT);
+
+
+
+	draw(100);
+
+	turn(90,RIGHT);
+
+	draw(100);
+
+	turn(90,LEFT);
+
+
 	
     display_drawing();
 
